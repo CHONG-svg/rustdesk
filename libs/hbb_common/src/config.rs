98ff1a1a8807651be323204f2b25dd6fb519fe47
CHONG-svg <384828606@qq.com>
2024-08-29 09:44:31 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.46.81.81"];
+pub const PUBLIC_RS_PUB_KEY: &str = "L3daS3vjbLpEMIwvcPlK0hW4SkJ29C7yfooqwVEUAMo";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
